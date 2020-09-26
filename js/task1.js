8ab43d52a3d91dfaf2741478a473c82230259d9d
Vasyl Saliuk <vace1986@gmail.com>
2020-09-26 17:20:35 +0300

    add index.html

---
+++ js/task1.js
@@ -0,0 +1,3 @@
+const name = '«Генератор защитного поля»'; // Write code on this line
+let price = 1000; // Write code on this line
+const firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`;
